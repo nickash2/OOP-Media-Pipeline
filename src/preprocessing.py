371bda3c2b7533e584c993606f1579502f4e220d
nickash2 <nickash015@gmail.com>
2024-02-01 16:44:22 +0100

    Finished the image preprocessing and started Audio

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import numpy as np
+from PIL import Image
 
 
 class AbtractPreprocessor(ABC):
@@ -15,24 +16,72 @@ class AbtractPreprocessor(ABC):
         pass
 
 
+# Images
 class CentreCrop(AbtractPreprocessor):
     def __init__(self, width, height):
         self.width_param = width
         self.height_param = height
 
     def __call__(self, img):
-        self.img = img
-        self.img = self._preprocess(img)
+        self.real_width, self.real_height = img.size
+        self.img = self._preprocess(Image.fromarray(img))
         return self.img
 
+    def _check_valid_size(self, img):
+        if (self.width_param < self.real_width and
+                self.height_param < self.real_height):
+            return True
+
     def _preprocess(self, img):
-        width, height = img.size
-        if self.width_param >= width or self.height_param >= height:
-            raise ValueError('Image is smaller than the crop size')
+        if self._check_valid_size(img):
+            return np.array(img)
 
-        left = max(0, (width - self.width_param)/2)
-        top = max(0, (height - self.height_param)/2)
-        right = min(width, left + self.width_param)
-        bottom = min(height, top + self.height_param)
+        left = max(0, (self.real_width - self.width_param) / 2)
+        top = max(0, (self.real_height - self.height_param) / 2)
+        right = min(self.real_width, left + self.width_param)
+        bottom = min(self.real_height, top + self.height_param)
 
-        return np.array(img.crop((left, top, right, bottom)).convert('RGB'))
+        return np.array(img.crop((left, top, right, bottom)))
+
+
+class RandomCrop(CentreCrop):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+    def _preprocess(self, img):
+        if self._check_valid_size(img):
+            return np.array(img)
+
+        left = np.random.randint(0, self.real_width - self.width_param + 1)
+        top = np.random.randint(0, self.real_height - self.height_param + 1)
+        right = left + self.width_param
+        bottom = top + self.height_param
+        return np.array(img.crop((left, top, right, bottom)))
+
+
+# Audio
+
+# Pitch shift
+class PitchShift(AbtractPreprocessor):
+    def __init__(self, hyperparameters):
+        pass
+
+    def __call__(self, audio):
+        self.audio = self._preprocess(audio)
+        return self.audio
+
+    def _preprocess(self, audio):
+        pass
+
+
+# mel spectrogram
+class MelSpectrogram(AbtractPreprocessor):
+    def __init__(self, hyperparameters):
+        pass
+
+    def __call__(self, audio):
+        self.audio = self._preprocess(audio)
+        return self.audio
+
+    def _preprocess(self, audio):
+        pass
